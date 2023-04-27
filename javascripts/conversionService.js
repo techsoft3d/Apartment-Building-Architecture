dbf6b6b7e717b3650f51d07edc455ce8aaceed64
Ricky Johnson <ricky.johnson@techsoft3d.com>
2023-04-27 16:41:31 -0400

    updated latest

--- javascripts/conversionService.js
+++ javascripts/conversionService.js
@@ -19,7 +19,7 @@ async function startViewer() {
                 containerId: "viewer-container",
                 endpointUri: endpointUriBeginning + data.serverurl + ":" + data.port + '?token=' + data.sessionid,
                 model: "arboleda",
-                enginePath: "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@latest",
+                enginePath: "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer",
                 rendererType: 0
         });
 
